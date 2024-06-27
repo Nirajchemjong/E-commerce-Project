0319944fe6d98453606e8b7ce6c963045d2ba391
Nirajchemjong <nirajlimbu7@gmail.com>
2024-06-27 21:53:48 +1000

    simple

--- eCommerce-Admin/api/controller/userController.js
+++ eCommerce-Admin/api/controller/userController.js
@@ -21,7 +21,6 @@ const registerUser = async (req, res, next) => {
     const verificationCode = uuidv4();
     req.body.verificationCode = verificationCode;
     await createAdmin(req.body);
-    console.log('-----Here----');
     const link = `${FE_URL}/admin-verification?c=${verificationCode}&e=${encodeURIComponent(email)}`;
     await sendAccountActivationEmail({ link, fName, email });
     res.json({
